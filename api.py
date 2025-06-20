05f52facf1775287c1a7ce367068387dce5af8bb
emlin <49819627+emlinhax@users.noreply.github.com>
2025-06-20 12:26:16 +0200

    Update api.py

--- api.py
+++ api.py
@@ -6,7 +6,7 @@ import arkhash
 def P(url):
     return (url, {"X-Payload": arkhash.generate_hash(url), "X-Timestamp": str(int(time.time()))})
 
-// example
+# example
 def get_balances(address):
     url, headers = P(f"https://api.arkm.com/balances/address/{address}")
     response = requests.get(url, headers=headers).json()
